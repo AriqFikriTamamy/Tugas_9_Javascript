157a14c2a8eb6ed5df1e670a82762b85ce82e0d7
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-07-07 18:56:38 +0700

    Tugas 9 Javascript

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function dataMahasiswa(){
+  var datadiri = {
+    nama: "Ariq",
+    jenis_kelamin: "laki-laki",
+    tempat_lahir: "Bandung",
+    tanggal_lahir: "2 September 2000",
+  }
+  for(var y in datadiri){
+    console.log(datadiri[y])
+  }
+}
+dataMahasiswa()
